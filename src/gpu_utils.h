55b35b16d087a7b9337c47485170dbc8fc609b86
ainozaki <aiai10piano@gmail.com>
2024-08-26 14:04:34 +0900

    define gpu_ptr

--- src/gpu_utils.h
+++ src/gpu_utils.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <cassert>
+
 #include <cuda.h>
 #include <cuda_runtime.h>
 
 namespace hifive {
+
+void __checkCudaErrors(cudaError_t err, const char *filename, int line);
+#define checkCudaErrors(err) __checkCudaErrors(err, __FILE__, __LINE__)
 
 class GPUContext {
 public:
@@ -13,5 +18,27 @@ public:
 
     int max_threads_per_block_;
 };
+
+template <typename T>
+class gpu_ptr {
+public:
+    gpu_ptr(T *d_ptr, uint64_t size) : d_ptr_(d_ptr), n_(size) {}
+    ~gpu_ptr() { cudaFree(d_ptr_); }
+
+    T *get() const { return d_ptr_; }
+    uint64_t size() const { return n_; }
+
+private:
+    T *d_ptr_ = nullptr;
+    uint64_t n_ = 0;
+};
+
+template <typename T>
+gpu_ptr<T> make_gpu_ptr(T *src, uint64_t size) {
+    T *d_ptr;
+    checkCudaErrors(cudaMalloc(&d_ptr, size * sizeof(T)));
+    checkCudaErrors(cudaMemcpy(d_ptr, src, size * sizeof(T), cudaMemcpyHostToDevice));
+    return gpu_ptr<T>(d_ptr, size);
+}
 
 } // namespace hifive
--- test/gpu_utils.cu
+++ test/gpu_utils.cu
@@ -2,7 +2,15 @@
 
 #include "gpu_utils.h"
 
-TEST(GPUUtils, GetGPUConfig) { 
+TEST(GPUUtils, GetGPUConfig) {
     hifive::GPUContext gpu;
     gpu.GetGPUInfo(true);
+}
+
+TEST(GPUUtils, MallocDeviceVector) {
+    uint64_t *h_vec = new uint64_t[10];
+    hifive::gpu_ptr<uint64_t> d_ptr = hifive::make_gpu_ptr<uint64_t>(h_vec, 10);
+    ASSERT_EQ(d_ptr.size(), 10);
+    delete[] h_vec;
+    // d_ptr will be automatically freed
 }
