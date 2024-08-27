7909e2cd6bf42f6f4c5b2ac92032746fa493c1fb
ainozaki <aiai10piano@gmail.com>
2024-08-27 10:38:36 +0900

    HMultRelin template

--- src/evaluate.cu
+++ src/evaluate.cu
@@ -72,6 +72,38 @@ void HMult(const seal::SEALContext &context, Ciphertext &result,
     }
 }
 
+void HMultRelin(const seal::SEALContext &context, Ciphertext &result,
+                const Ciphertext &ct0, const Ciphertext &ct1,
+                const gpu_ptr &modulus) {
+    // Verify parameters.
+    if (ct0.coeff_modulus_size() != ct1.coeff_modulus_size()) {
+        throw std::invalid_argument("ct0 and ct1 parameter mismatch");
+    }
+    if (ct0.poly_modulus_degree() != ct1.poly_modulus_degree()) {
+        throw std::invalid_argument("ct0 and ct1 parameter mismatch");
+    }
+    if ((!ct0.is_ntt_form()) | (!ct1.is_ntt_form())) {
+        throw std::invalid_argument("Must be in NTT form");
+    }
+
+    const uint64_t poly_modulus_degree = ct0.poly_modulus_degree();
+    const uint64_t coeff_modulus_size = ct0.coeff_modulus_size();
+    dim3 gridSize(1024, 1, 1);
+    dim3 blockSize(poly_modulus_degree / 1024, 1, 1);
+    for (int i = 0; i < coeff_modulus_size; i++) {
+        uint64_t *ct0_axi = ct0.ax() + i * poly_modulus_degree;
+        uint64_t *ct1_axi = ct1.ax() + i * poly_modulus_degree;
+        uint64_t *ct0_bxi = ct0.bx() + i * poly_modulus_degree;
+        uint64_t *ct1_bxi = ct1.bx() + i * poly_modulus_degree;
+        uint64_t *result_axi = result.ax() + i * poly_modulus_degree;
+        uint64_t *result_bxi = result.bx() + i * poly_modulus_degree;
+        poly_mult_mod<<<gridSize, blockSize>>>(result_axi, ct0_axi, ct1_axi,
+                                               modulus.get(), i);
+        poly_mult_mod<<<gridSize, blockSize>>>(result_bxi, ct0_bxi, ct1_bxi,
+                                               modulus.get(), i);
+    }
+}
+
 void NTT(DNTTTable &d_ntt_table, gpu_ptr &a, int batch_size, int start_idx) {
     const auto &s = cudaStreamPerThread;
     nwt_2d_radix8_forward_inplace(a.get(), d_ntt_table, batch_size, 0, s);
--- src/evaluate.h
+++ src/evaluate.h
@@ -20,6 +20,10 @@ void HMult(const seal::SEALContext &context, Ciphertext &result,
            const Ciphertext &ct0, const Ciphertext &ct1,
            const gpu_ptr &modulus);
 
+void HMultRelin(const seal::SEALContext &context, Ciphertext &result,
+                const Ciphertext &ct0, const Ciphertext &ct1,
+                const gpu_ptr &modulus);
+
 void NTT(DNTTTable &d_ntt_table, gpu_ptr &a, int batch_size, int start_idx);
 
 void iNTT(DNTTTable &d_ntt_table, gpu_ptr &a, int batch_size, int start_idx);
