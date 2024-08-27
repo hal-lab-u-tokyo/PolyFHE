05b9e7300879568c1cbf9e295e606f56d2323162
ainozaki <aiai10piano@gmail.com>
2024-08-27 18:51:05 +0900

    add polynomial dialect

---
+++ lib/Dialect/Polynomial/PolynomialDialect.cc
@@ -0,0 +1,15 @@
+#include "lib/Dialect/Polynomial/PolynomialDialect.h"
+
+#include "lib/Dialect/Polynomial/PolynomialDialect.cpp.inc"
+
+namespace mlir {
+namespace hifive {
+namespace Polynomial {
+
+void PolynomialDialect::initialize() {
+  // This is where we will register types and operations with the dialect
+}
+
+}  // namespace Polynomial
+}  // namespace hifive
+}  // namespace mlir
