30eca5745f054b7b00ff9846ec026511ae747e2c
ainozaki <aiai10piano@gmail.com>
2025-01-06 18:11:28 +0900

    E2E exectime plot

---
+++ profile/plot-e2e-exectime.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+directory_path = "/opt/mount/HiFive"
+
+benchmarks = ("Sobel Filter", "Linear Regression", "Multi-Layer Perceptron")
+
+works = {
+    'SEAL(CPU)': (18.35, 18.43, 14.98),
+    '100x(CPU)': (38.79, 48.83, 47.50),
+    'Phantom(GPU)': (189.95, 195.82, 217.19),
+    'This Work(GPU)': (189.95, 195.82, 217.19),
+}
+
+x = np.arange(len(benchmarks))
+width = 0.2
+multiplier = 0
+
+fig, ax = plt.subplots(figsize=(12, 8))
+
+for attribute, measurement in works.items():
+    offset = width * multiplier
+    rects = ax.bar(x + offset, measurement, width, label=attribute)
+    ax.bar_label(rects, padding=3)
+    multiplier += 1
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Execution Time (ms)', fontsize=24)
+ax.set_xticks(x + width * 1.5, benchmarks, fontsize=18)
+ax.legend(loc='upper left', ncols=2, fontsize=14)
+ax.set_ylim(0, 300)
+
+plt.savefig(f"{directory_path}/profile/figure/e2e-exectime.png", dpi=500)
+print(f"Figure saved at {directory_path}/profile/figure/e2e-exectime.png")
