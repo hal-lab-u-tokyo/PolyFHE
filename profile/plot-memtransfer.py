f35f88b33a64f4e612a393038a2e06d93645c0e4
ainozaki <aiai10piano@gmail.com>
2025-01-10 01:35:59 +0900

    data transfer evalation

--- profile/plot-memtransfer.py
+++ profile/plot-memtransfer.py
@@ -8,14 +8,19 @@ directory_path = "/opt/mount/HiFive"
 
 data_opt = {}
 data_noopt = {}
-count_opt = {}
-count_noopt = {}
+data_phantom = {}
+datas = [data_opt, data_noopt, data_phantom]
+
 
 # Read CSV
 fname_opt = "profile/data/memtransfer-opt.csv"
 fname_noopt = "profile/data/memtransfer-noopt.csv"
+fname_phantom = "profile/data/memtransfer-phantom.csv"
+files = [fname_opt, fname_noopt, fname_phantom]
 
-for fname in [fname_opt, fname_noopt]:
+for idx in range(len(files)):
+    fname = files[idx]
+    data = datas[idx]
     filepath = os.path.join(directory_path, fname)
 
     if not os.path.exists(filepath):
@@ -29,62 +34,39 @@ for fname in [fname_opt, fname_noopt]:
         metric_name_idx = l[0].index("Metric Name")
         metric_value_idx = l[0].index("Metric Value")
 
-        if fname == fname_opt:
-            data = data_opt
-            count = count_opt
-        else:
-            data = data_noopt
-            count = count_noopt
-
         for i in range(1, len(l)):
             entry = l[i]
             # Metric name
             metric_name = entry[metric_name_idx]
-            if metric_name == "l1tex__t_sector_hit_rate.pct":
-                metric_name = "L1 Cache Hit Rate"
-            elif metric_name == "lts__t_sector_hit_rate.pct":
-                metric_name = "LTS Cache Hit Rate"
-            # Metric value
             metric_value = float(entry[metric_value_idx])
 
             
             if metric_name not in data:
-                data[metric_name] = metric_value
-                count[metric_name] = 1
+                data[metric_name] = metric_value / 1000
             else:
-                data[metric_name] += metric_value
-                count[metric_name] += 1
+                data[metric_name] += metric_value / 1000
 
-        # Normalize
-        for metric_name, metric_value in data.items():
-            data[metric_name] = metric_value / count[metric_name]
 
 print("Optimized")
-print(data_opt)
+print(datas[0])
 print("Non-optimized")
-print(data_noopt)
+print(datas[1])
+print("Phantom")
+print(datas[2])
 
 # Plot
 fig, ax = plt.subplots(figsize=(18, 10))
-metrics = list(data_opt.keys())
-values = {"Optimized": [data_opt[metric] for metric in metrics], "Baseline": [data_noopt[metric] for metric in metrics]}
+metric = "dram__bytes_read.sum"
+candidates = ["ThisWork(Baseline)", "ThisWork", "Phantom"]
+result = [datas[i][metric] for i in range(len(datas))]
 
-print(metrics)
-print(values)
+print(metric)
+print(candidates)
+print(result)
 
-x = np.arange(len(metrics))
-width = 0.25
-multiplier = 0
-for label, v in values.items():
-    offset = width * multiplier
-    rects = ax.bar(x + offset, v, width, label=label)
-    ax.bar_label(rects, padding=2, fontsize=20)
-    multiplier += 1
-
-ax.legend(fontsize=24, loc='best')
-ax.set_ylabel("Bytes", fontsize=24)
+ax.bar(candidates, result, color='tab:blue')
+ax.set_ylabel("Data Transfer [MB]", fontsize=24)
 ax.tick_params(axis='y', labelsize=24)
-ax.set_xticks(x + width/2, labels=metrics, fontsize=24)
-ax.set_title("Optimization Impact on Global Memory Transfer", fontsize=24)
+ax.tick_params(axis='x', labelsize=24)
 plt.savefig(f"{directory_path}/profile/figure/memtransfer.png", dpi=500)
 print(f"Figure saved at {directory_path}/profile/figure/memtransfer.png")
--- profile/profile-memtransfer.sh
+++ profile/profile-memtransfer.sh
@@ -14,10 +14,13 @@ cd $REPO_ROOT
 #METRICS="l1tex__t_sector_hit_rate.pct,lts__t_sector_hit_rate.pct,l1tex__t_requests_pipe_lsu_mem_global_op_ld.sum,l1tex__t_sectors_pipe_lsu_mem_global_op_st.sum"
 METRICS="dram__bytes_read.sum"
 
-ncu -f -o opt --csv --metrics "${METRICS}" ./build-opt/bench
-ncu --csv --import opt.ncu-rep > profile/data/memtransfer-opt.csv
+ncu -f -o memtransfer-opt --csv --metrics "${METRICS}" ./build-opt/bench
+ncu --csv --import memtransfer-opt.ncu-rep > profile/data/memtransfer-opt.csv
 
-ncu -f -o noopt --csv --metrics "${METRICS}" ./build-noopt/bench
-ncu --csv --import noopt.ncu-rep > profile/data/memtransfer-noopt.csv
+ncu -f -o memtransfer-noopt --csv --metrics "${METRICS}" ./build-noopt/bench
+ncu --csv --import memtransfer-noopt.ncu-rep > profile/data/memtransfer-noopt.csv
+
+ncu -f -o memtransfer-phantom --csv --metrics "${METRICS}" ./thirdparty/phantom-fhe/build-for-eval/ckks_hmult_logn15_k4
+ncu --csv --import memtransfer-phantom.ncu-rep > profile/data/memtransfer-phantom.csv
 
 python3 ./profile/plot-memtransfer.py
