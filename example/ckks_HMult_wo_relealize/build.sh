a227c0fa21240605ad1e5c4fff75f46bd92a188a
ainozaki <aiai10piano@gmail.com>
2025-05-13 17:01:09 +0900

    add example wo relinalize

---
+++ example/ckks_HMult_wo_relealize/build.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+set -ex
+
+OPT_LEVEL=$1
+if [ -z "$OPT_LEVEL" ]; then
+  echo "Usage: $0 <opt_level>"
+  echo "Example: $0 noopt"
+  exit 1
+fi
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+cd $REPO_ROOT/example/ckks_HMult_wo_relealize
+uv run ./example.py
+make clean
+make OPT_LEVEL=--$OPT_LEVEL
+make dot
