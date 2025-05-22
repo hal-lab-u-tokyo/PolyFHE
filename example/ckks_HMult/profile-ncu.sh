ee706a041cef8fff2607d8b9c69e8d1fd8f4cddf
ainozaki <aiai10piano@gmail.com>
2025-05-22 11:22:42 +0900

    v26 good

--- example/ckks_HMult/profile-ncu.sh
+++ example/ckks_HMult/profile-ncu.sh
@@ -22,5 +22,5 @@ for NUM_DIVIDES in ${NUM_DIVIDES_LIST[@]}; do
     echo "NUM_DIVIDES=${NUM_DIVIDES}"
     rm -f cachehit.ncu-rep
     ncu -f -o cachehit --nvtx --nvtx-include "compute/" --csv --metrics "${METRICS}" ./build/example.out ${NUM_DIVIDES}
-    ncu --csv --import cachehit.ncu-rep > data/ncu-v25-${NUM_DIVIDES}.csv
+    ncu --csv --import cachehit.ncu-rep > data/ncu-v26-${NUM_DIVIDES}.csv
 done
